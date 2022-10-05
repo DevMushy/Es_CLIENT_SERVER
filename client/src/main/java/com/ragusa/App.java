695f924c652264ba7c438b11bc434c3bd484adbc
lai2-01 <lai2-01@itismeucci.com>
2022-10-05 09:46:23 +0200

    commit server e client finito

--- client/src/main/java/com/ragusa/App.java
+++ client/src/main/java/com/ragusa/App.java
@@ -1,9 +1,25 @@
 package com.ragusa;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        Socket s = new Socket(InetAddress.getLocalHost(), 3000);
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        pr.println("ci sono");
+        pr.flush();
+
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader br = new BufferedReader(in);
+
+        String str = br.readLine();
+        System.out.println("Server: " + str);
+        s.close();
     }
 }
